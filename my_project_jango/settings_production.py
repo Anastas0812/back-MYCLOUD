103954e1b510d3972fc56e9ec60ecfd629389bbb
anastasy_klimanova <klimanova_anastasy@mail.ru>
2026-05-19 02:01:23 +0300

    fix config

--- my_project_jango/settings_production.py
+++ my_project_jango/settings_production.py
@@ -1,16 +1,25 @@
+import os
 from .settings_local import *
 
 DEBUG = False
 
 ALLOWED_HOSTS = [
-    'домен.ru',
-    'www.твой_домен.ru',
-    'IP_адрес_сервера',
+    'localhost',
+    '168.222.142.98',
 ]
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 SECURE_BROWSER_XSS_FILTER = True
 CSRF_TRUSTED_ORIGINS = [
-    'https://твой_домен.ru',
+    'localhost',
+    '168.222.142.98',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://168.222.142.98',
 ]

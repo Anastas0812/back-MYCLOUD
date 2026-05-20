14f867a8bb0637f4eb9c07e067caa740a88faffc
anastasy_klimanova <klimanova_anastasy@mail.ru>
2026-05-20 15:23:22 +0300

    fix: remove sqlite and fix gitignore, fix: settings CSRF

--- users/authentication.py
+++
@@ -1,9 +0,0 @@
-#SessionAuthentication в DRF принудительно проверяет CSRF.
-#класс аутентификации который пропускает CSRF проверку
-
-from rest_framework.authentication import SessionAuthentication
-
-
-class CsrfExemptSessionAuthentication(SessionAuthentication):
-    def enforce_csrf(self, request):
-        return  #пропускаем проверку CSRF
